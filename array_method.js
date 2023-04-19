29bf3cf877b382ed211a537513b9e627cc94b802
hyunin3 <stock4share2@gmail.com>
2023-04-19 16:19:33 +0900

    0419

---
+++ array_method.js
@@ -0,0 +1,20 @@
+const numbers = [1, 2, 3, 4, 5]
+
+// array.reverse()
+numbers.reverse()
+console.log(numbers)
+
+// array.push(values) array.pop()
+numbers.push(100)
+console.log(numbers)
+
+numbers.pop()
+console.log(numbers)
+
+// array.includes(values)
+console.log(numbers.includes(1))
+console.log(numbers.includes(100))
+
+// array.indexOf(value)
+console.log(numbers.indexOf(3))
+console.log(numbers.indexOf(100))
